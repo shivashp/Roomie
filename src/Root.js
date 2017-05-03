86200f4f13fd6e511fe7550fe17e9cc35f8c3d4d
shivashp <shivapandey04@gmail.com>
2017-05-03 12:19:16 +0545

    Changes

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import Login from './components/Login';
+import Home from './components/Home';
 
 const Root = () => {
   return (
-    <Router key="root">
-      <Scene key="login" hideNavBar={true} component={Login} title="Login" />
+    <Router key="root" hideNavBar={true} >
+      <Scene key="home" component={Home} title="Home" />
+      <Scene key="login" component={Login} title="Login" />
     </Router>
   )
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+  View, Text, StyleSheet, Image, TouchableOpacity
+} from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Home</Text>
+      </View>
+    )
+  }
+}
